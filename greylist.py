514afe58621441a7dc8e6d7a14632ea9a4f2302f
Jonas Wielicki <j.wielicki@sotecware.net>
2014-05-10 14:28:09 +0200

    Implement config file and usable ui

--- greylist.py
+++ greylist.py
@@ -4,7 +4,20 @@ import sqlite3
 
 from datetime import datetime, timedelta
 
-logger = logging.getLogger("greylist")
+class RESPONSE:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "<response={}>".format(self.name)
+
+PASSED = RESPONSE("PASSED")
+FAILED = RESPONSE("FAILED")
+
+del RESPONSE
 
 # CONFIGURATION
 
@@ -12,6 +25,9 @@ logger = logging.getLogger("greylist")
 # whitelisting happens on client_name
 # client name is either the client_name (hostname) supplied or the
 # client_address, if no name was supplied
+
+# these values are defaults. they can be configured by passing a INI-style
+# config file via --config, in the [DEFAULT] section
 
 # If more than the given number of requests have been made by a given
 # client_name which have passed the greylisting check, the address is added to
@@ -45,6 +61,7 @@ stats_active_threshold = 3600
 
 # action=dunno will only permit if no other rule denies
 response_pass = "action=dunno\n"
+
 # action=defer_if_permit will defer the mail if no rule denies it
 response_fail = "action=defer_if_permit You have been greylisted.\n"
 
@@ -69,8 +86,9 @@ SCHEMA[("table", "greylist")] = """CREATE TABLE greylist
       last_seen TIMESTAMP,
       CONSTRAINT match UNIQUE (client_name, sender, recipient)
   )"""
-SCHEMA[("index", "whitelist_last_seen")] = """CREATE INDEX last_seen ON greylist (last_seen)"""
-SCHEMA[("index", "greylist_last_seen")] = """CREATE INDEX last_seen ON greylist (last_seen)"""
+SCHEMA[("index", "whitelist_last_seen")] = """CREATE INDEX whitelist_last_seen ON greylist (last_seen)"""
+SCHEMA[("index", "greylist_last_seen")] = """CREATE INDEX greylist_last_seen ON greylist
+(last_seen)"""
 
 _dbconn = None
 
@@ -92,7 +110,7 @@ def create_db(dbconn):
     indicies = ((index, sql)
                 for (type_, index), sql in SCHEMA.items()
                 if type_ == "index")
-    for index, sql in indices:
+    for index, sql in indicies:
         logger.info("creating index %s", index)
         dbconn.execute(sql)
         logger.info("created index %s", index)
@@ -105,6 +123,29 @@ def get_db():
                                   detect_types=sqlite3.PARSE_DECLTYPES)
         setup_db(_dbconn)
     return _dbconn
+
+def getint_or_none(config, section, option, fallback):
+    try:
+        v = config.get(section, option).lower()
+    except configparser.NoOptionError:
+        return fallback
+    if v in {"none", "off", "disabled"}:
+        return None
+    return int(v)
+
+def getresponse(config, section, option, fallback):
+    try:
+        v = config.get(section, option).upper()
+    except configparser.NoOptionError:
+        return fallback
+
+    try:
+        return {
+            "PASSED": PASSED,
+            "FAILED": FAILED
+        }[v]
+    except ValueError:
+        raise ValueError("Invalid response type: {}".format(v))
 
 def gc_db():
     dbconn = get_db()
@@ -159,6 +200,54 @@ def gc_db():
             dbconn.commit()
         cursor.close()
 
+def load_config(f):
+    global auto_whitelist_threshold, max_greylist_entries
+    global max_whitelist_entries, greylist_expire, whitelist_expire
+    global stats_active_threshold, response_pass, response_fail
+    config = configparser.ConfigParser()
+    with f as f:
+        config.read_file(f)
+
+    auto_whitelist_threshold = getint_or_none(
+        config,
+        "DEFAULT", "auto_whitelist_threshold",
+        fallback=auto_whitelist_threshold)
+
+    max_greylist_entries = getint_or_none(
+        config,
+        "DEFAULT", "max_greylist_entries",
+        fallback=max_greylist_entries)
+
+    max_whitelist_entries = getint_or_none(
+        config,
+        "DEFAULT", "max_whitelist_entries",
+        fallback=max_whitelist_entries)
+
+    greylist_expire = getint_or_none(
+        config,
+        "DEFAULT", "greylist_expire",
+        fallback=greylist_expire)
+
+    whitelist_expire = getint_or_none(
+        config,
+        "DEFAULT", "whitelist_expire",
+        fallback=whitelist_expire)
+
+    stats_active_threshold = getint_or_none(
+        config,
+        "DEFAULT", "stats_active_threshold",
+        fallback=stats_active_threshold)
+
+    response_pass = getint_or_none(
+        config,
+        "DEFAULT", "response_pass",
+        fallback=response_pass)
+
+    response_fail = getint_or_none(
+        config,
+        "DEFAULT", "response_fail",
+        fallback=response_fail)
+
 def read_request(instream):
     attrs = {}
     for line in map(str.strip, instream):
@@ -202,7 +291,7 @@ def _check_greylist(dbconn, cursor, sender, recipient, client_name):
                        key + (now, now))
         cursor.close()
         dbconn.commit()
-        return False
+        return FAILED
     else:
         first_seen, = match
         logger.debug("greylist check: match, first_seen=%s", first_seen)
@@ -217,14 +306,14 @@ INSERT OR IGNORE INTO whitelist (client_name, hit_count) VALUES (?, 0);""",
 UPDATE whitelist SET last_seen = ?, hit_count = hit_count + 1;""",
                            (now,))
             dbconn.commit()
-            return True
+            return PASSED
         cursor.execute("""UPDATE greylist
         SET last_seen = ?
         WHERE sender=? AND recipient=? AND client_name=?""",
                        (now,) + key)
         dbconn.commit()
         logger.debug("greylist check: defer")
-        return False
+        return FAILED
 
 def process_request(attrs):
     dbconn = get_db()
@@ -238,7 +327,7 @@ def process_request(attrs):
         logger.debug("processing request: sender=%r, recipient=%r, client_name=%r",
                       sender, recipient, client_name)
         if _check_whitelist(cursor, client_name):
-            return True
+            return PASSED
 
         return _check_greylist(dbconn, cursor, sender, recipient, client_name)
     finally:
@@ -274,3 +363,69 @@ def verify_db(dbconn):
             raise ValueError("Missing tables: {}".format(", ".join(missing)))
     finally:
         cursor.close()
+
+if __name__ == "__main__":
+    import argparse
+    import configparser
+    import logging
+    import sys
+
+    parser = argparse.ArgumentParser(
+        description="""Accept Postfix policy check requests on stdin and perform
+        greylisting. Returns an action code depending on greylisting result. For
+        configuration details please see the comments at the start of
+        greylist.py"""
+    )
+    parser.add_argument(
+        "-c", "--config",
+        default=None,
+        type=argparse.FileType("r"),
+        metavar="FILE",
+        help="Specify a config file to override defaults")
+    parser.add_argument(
+        "-v",
+        dest="verbosity",
+        action="count",
+        default=0,
+        help="Increase verbosity by one step")
+
+    args = parser.parse_args()
+
+    verbosity = {
+        0: logging.ERROR,
+        1: logging.WARN,
+        2: logging.INFO,
+        3: logging.DEBUG
+    }
+
+    logging.basicConfig(
+        level=verbosity.get(args.verbosity, logging.DEBUG),
+        stream=sys.stderr)
+    logger = logging.getLogger("greylist")
+
+    if args.config is not None:
+        load_config(args.config)
+
+    try:
+        while True:
+            request = read_request(sys.stdin)
+            if request is None:
+                break
+
+            clean_request(request)
+            response = process_request(request)
+            if response == PASSED:
+                print(response_pass)
+            elif response == FAILED:
+                print(response_fail)
+            else:
+                raise AssertionError("Programming error")
+
+            gc_db()
+    except KeyboardInterrupt:
+        pass
+
+else:
+    # defer configuration of logger until the end
+    import logging
+    logger = logging.getLogger("greylist")
